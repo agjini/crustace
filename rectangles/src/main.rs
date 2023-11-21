08842a0afa0235b0e9db88099d265694aa00a4b1
Augustin Gjini <augustin.gjini@pm.me>
2023-11-21 13:53:13 +0100

    7 eme chapitre

---
+++ rectangles/src/main.rs
@@ -0,0 +1,47 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+    fn perimeter(&self) -> u32 {
+        self.area() * 2
+    }
+
+    fn can_hold(&self, rectangle: &Self) -> bool {
+        self.width > rectangle.width && self.height > rectangle.height
+    }
+
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+}
+
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    println!("square created ? {:?}", Rectangle::square(4));
+}
+
+//https://doc.rust-lang.org/book/ch06-00-enums.html
