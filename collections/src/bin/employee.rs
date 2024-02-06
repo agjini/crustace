81cfcd85fc5c2b5170d9a2e9af033865854d6415
Augustin Gjini <augustin.gjini@pm.me>
2024-02-06 14:13:00 +0100

    15 eme

--- collections/src/bin/employee.rs
+++ collections/src/bin/employee.rs
@@ -9,18 +9,46 @@ struct Company {
 }
 
 impl Company {
+    pub fn new(people_by_department: HashMap<String, Vec<String>>) -> Company {
+        let mut entries: Vec<(&String, &Vec<String>)> = people_by_department.iter().collect();
+        entries.sort_by_key(|e| e.0);
+
+        for entry in entries {
+            let mut people = entry.1;
+            let mut cloned = people.clone();
+            cloned.sort();
+        }
+
+        Self {
+            people_by_department,
+        }
+    }
+
     pub fn all_people_in_the_company_by_department(&self) -> String {
         let mut all_people = "".to_string();
         let mut entries: Vec<(&String, &Vec<String>)> = self.people_by_department.iter().collect();
-        entries.sort_by_key(|e| e.0);
+        //entries.sort_by_key(|e| e.0);
         for (department, peoples) in entries {
-            let s = format!("- {} : {}", department, peoples.join(", "));
+            let mut sorted_peoples = peoples.clone();
+            //sorted_peoples.sort();
+            let s = format!("- {} : {}", department, sorted_peoples.join(", "));
             if !all_people.is_empty() {
                 all_people.push('\n');
             }
             all_people.push_str(s.as_str());
         }
         all_people
+    }
+    pub fn get_by_department(&self, department: String) -> String {
+        let people = self.people_by_department.get(&department);
+        match people {
+            None => "".to_string(),
+            Some(p) => {
+                let mut ret = p.clone();
+                ret.sort();
+                ret.join(", ")
+            }
+        }
     }
 }
 
@@ -30,9 +58,7 @@ mod test {
 
     #[test]
     fn get_people_by_department_when_empty() {
-        let company = Company {
-            people_by_department: HashMap::new(),
-        };
+        let company = Company::new(HashMap::new());
         assert_eq!("", company.all_people_in_the_company_by_department());
     }
 
@@ -50,17 +76,15 @@ mod test {
     }
 
     #[test]
-    fn get_people_by_department_when_filled_with_more_than_Sally() {
+    fn get_people_by_department_when_filled_with_more_than_sally() {
         let mut people_by_department = HashMap::new();
         people_by_department.insert(
             "Sales".to_string(),
             vec!["Sally".to_string(), "Marc".to_string()],
         );
-        let company = Company {
-            people_by_department,
-        };
+        let company = Company::new(people_by_department);
         assert_eq!(
-            "- Sales : Sally, Marc",
+            "- Sales : Marc, Sally",
             company.all_people_in_the_company_by_department()
         );
     }
@@ -76,11 +100,9 @@ mod test {
             "Devs".to_string(),
             vec!["Bill".to_string(), "Ned".to_string()],
         );
-        let company = Company {
-            people_by_department,
-        };
+        let company = Company::new(people_by_department);
         assert_eq!(
-            "- Devs : Bill, Ned\n- Sales : Sally, Marc",
+            "- Devs : Bill, Ned\n- Sales : Marc, Sally",
             company.all_people_in_the_company_by_department()
         );
     }
@@ -97,12 +119,29 @@ mod test {
             vec!["Bill".to_string(), "Ned".to_string()],
         );
         people_by_department.insert("Artisans".to_string(), vec!["Booby".to_string()]);
-        let company = Company {
-            people_by_department,
-        };
+        let company = Company::new(people_by_department);
         assert_eq!(
-            "- Artisans : Booby\n- Devs : Bill, Ned\n- Sales : Sally, Marc",
+            "- Artisans : Booby\n- Devs : Bill, Ned\n- Sales : Marc, Sally",
             company.all_people_in_the_company_by_department()
         );
+    }
+
+    #[test]
+    fn get_sales_people() {
+        let mut people_by_department = HashMap::new();
+        people_by_department.insert(
+            "Sales".to_string(),
+            vec!["Sally".to_string(), "Marc".to_string()],
+        );
+        people_by_department.insert(
+            "Devs".to_string(),
+            vec!["Bill".to_string(), "Ned".to_string()],
+        );
+
+        let company = Company::new(people_by_department);
+
+        let result = company.get_by_department("Sales".to_string());
+
+        assert_eq!("Marc, Sally", result)
     }
 }
