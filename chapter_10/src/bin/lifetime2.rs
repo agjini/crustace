aefe21091493d56e1c921db906f77dc9770f9cb7
Augustin Gjini <augustin.gjini@pm.me>
2025-03-26 14:13:06 +0100

    ch 11.1

--- chapter_10/src/bin/lifetime2.rs
+++ chapter_10/src/bin/lifetime2.rs
@@ -1,17 +1,27 @@
 fn main() {
     let string1 = String::from("abcd");
 
-    let result;
     {
         let string2 = String::from("xyz");
-        result = longest(string1.as_str(), &string2);
+        main_2(string1.as_str(), &string2);
     }
-
-    println!("The longest string is {result}");
 }
 
-fn longest<'a, 'b>(x: &'a str, y: &'b str) -> &'a str {
-    if x.len() > y.len() { x } else { y }
+fn main_2<'a>(x: &'a str, y: &'a str) -> &'a str {
+    let _ = longest(x, y);
+    x
+}
+
+// fn longest<'a, 'b>(x: &'a str, y: &'a str) -> &'a str {
+//     if x.len() > y.len() { x } else { y }
+// }
+
+fn longest<'a>(x: &'a str, y: &'a str) -> String {
+    "really long string".into()
+}
+
+fn longest_bis<'a, 'b>(x: &'a str, y: &'b str) -> String {
+    "really long string".into()
 }
 
 fn longestdd(x: &str) -> &str {
