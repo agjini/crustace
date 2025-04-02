300f3e28e4580e390d4fee5e986c389eb3d5d173
Augustin Gjini <augustin.gjini@pm.me>
2025-04-02 15:03:03 +0200

    chap 11 done

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -4,12 +4,24 @@ pub struct Guess {
 
 impl Guess {
     pub fn new(value: i32) -> Guess {
-        if value < 1  || value > 100{
+        if value < 1 || value > 100 {
             panic!("Guess value must be between 1 and 100, got {value}.");
         }
 
         Guess { value }
     }
+}
+
+fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+pub fn add_two(a: i32) -> i32 {
+    add(a, 2)
+}
+fn prints_and_returns_10(a: i32) -> i32 {
+    println!("I got the value {a}");
+    10
 }
 
 #[cfg(test)]
@@ -26,5 +38,36 @@ mod tests {
     #[should_panic]
     fn less_than_1() {
         Guess::new(-10);
+    }
+
+    #[test]
+    fn it_works_with_assert() {
+        let result = add(2, 2);
+
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn it_works_with_result() -> Result<(), String> {
+        let result = add(2, 2);
+
+        if result == 4 {
+            Ok(())
+        } else {
+            Err(String::from("two plus two does not equal four"))
+        }
+    }
+
+    #[test]
+    fn this_test_will_pass() {
+        let value = prints_and_returns_10(4);
+        assert_eq!(value, 10);
+    }
+
+    #[test]
+    #[ignore]
+    fn this_test_will_fail() {
+        let value = prints_and_returns_10(8);
+        assert_eq!(value, 5);
     }
 }
