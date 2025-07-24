0ad24db4bfa2ce7fb0eed9a823bf162d6b357823
Augustin Gjini <augustin.gjini@pm.me>
2025-07-24 14:17:20 +0200

    ch15-02

--- cons/src/main.rs
+++ cons/src/main.rs
@@ -1,11 +1,50 @@
 // construct function
 
+// https://doc.rust-lang.org/book/ch15-02-deref.html#implicit-deref-coercions-with-functions-and-methods
+
 enum List {
-    Cons(i32, Box<List>),
-    // Cons(i32, List),
     Nil,
+    Cons(i32, Box<List>),
 }
 
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> std::ops::Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    // let x = 5;
+    // let y = MyBox::new(x);
+    //
+    // let test = "toto";
+    // let test_to_string = test.to_string();
+    // let ref_test = &test_to_string;
+    // let ref_test_2: &str = &test_to_string;
+    //
+    // assert_eq!(5, x);
+    // assert_eq!(5, *y);
+
+    let m = MyBox::new(String::from("Rust"));
+    hello(&m);
+}
+
+fn hello(name: &str) {
+    println!("Hello, {name}!");
 }
--- smart_pointers/src/main.rs
+++ smart_pointers/src/main.rs
@@ -1,19 +1,28 @@
 #[derive(Copy, Clone, Debug)]
 struct PaiPai {
     age: i32,
-    son: Option<PaiPai>,
+    //son: Option<PaiPai>,
+    thunes: [i32; 10_000_000],
 }
 
 fn main() {
-    let b = Box::new(5); //heap
-    let s = *b;
-    println!("b = {s}");
-
-    let b = Box::new(PaiPai {
+    let pai = PaiPai {
         age: 18,
-        son: Some(PaiPai { age: 0, son: None }),
-    }); //heap
-    let s = b.clone();
-    println!("b = {b:?}");
-    println!("s = {s:?}");
+        thunes: [0; 10_000_000],
+    };
+    let pai = Box::new(pai);
+
+    println!("{:?}", pai.age);
+
+    // let b = Box::new(5); //heap
+    // let s = *b;
+    // println!("b = {s}");
+    //
+    // let b = Box::new(PaiPai {
+    //     age: 18,
+    //     son: Some(PaiPai { age: 0, son: None }),
+    // }); //heap
+    // let s = b.clone();
+    // println!("b = {b:?}");
+    // println!("s = {s:?}");
 }
