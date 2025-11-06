2fa1099aa67a3caa1b3e3bcdc98029314623d1c3
Augustin Gjini <augustin.gjini@pm.me>
2025-11-06 13:43:03 +0100

    channel

--- thread/src/main.rs
+++ thread/src/main.rs
@@ -1,68 +1,40 @@
 use std::sync::mpsc;
-use std::sync::mpsc::Sender;
 use std::thread;
-
-fn _main2() {
-    let v = vec![1, 2, 3];
-
-    let handle = thread::spawn(move || {
-        println!("Here's a vector: {v:?}");
-    });
-
-    handle.join().unwrap();
-}
+use std::time::Duration;
 
 fn main() {
-    let mut a = "hello".to_string();
-
-    let c = if true {
-        let a = 2;
-        a + 33
-    } else {
-        let a = 20;
-        a + 33
-    };
-    let b = &mut a;
-
-    println!("a : {a}, b: {b}, c: {c}");
-    //b.push_str(" world");
-
-    // qqch(&mut a); // copy ref
-
-    // println!("b : {a}");
-}
-
-fn qqch(s: &mut String) -> i32 {
-    println!("s : {s}");
-    s.push_str("papay");
-    33;
-}
-
-fn main3() {
     let (tx, rx) = mpsc::channel();
 
-    thread::spawn(move || tt(&tx));
+    let tx1 = tx.clone();
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("1"),
+            String::from("2"),
+            String::from("3"),
+            String::from("4"),
+        ];
 
-    tx.send(String::from("hello")).unwrap();
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
 
-    // thread::spawn(|| {
-    //     let val = String::from("hi");
-    //     let val2 = String::from("ho");
-    //     tx.send(val).unwrap();
-    //     thread::sleep(std::time::Duration::from_secs(15));
-    //     tx.send(val2).unwrap();
-    // });
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("5"),
+            String::from("6"),
+            String::from("7"),
+            String::from("8"),
+        ];
 
-    let toto = rx.recv().unwrap();
-    println!("Got: {}", toto);
-    let tata = rx.recv().unwrap();
-    println!("Got: {}", tata);
-}
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
 
-fn tt(tx: &Sender<String>) {
-    let val = String::from("hi");
-    let val2 = String::from("ho");
-    tx.send(val).unwrap();
-    thread::sleep(std::time::Duration::from_secs(15));
-    tx.send(val2).unwrap();
+    for received in rx {
+        println!("Got: {received}");
+    }
 }
