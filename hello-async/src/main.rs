4b00c1974efff6c0ba92213ccb40ebe7e32ebfb7
Augustin Gjini <augustin.gjini@pm.me>
2025-12-04 14:02:17 +0100

    start async concurrency

--- hello-async/src/main.rs
+++ hello-async/src/main.rs
@@ -1,29 +1,26 @@
-use trpl::{Either, Html};
+use std::time::Duration;
 
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
-
     trpl::run(async {
-        let title_fut_1 = page_title(&args[1]);
-        let title_fut_2 = page_title(&args[2]);
-
-        let (url, maybe_title) = match trpl::race(title_fut_1, title_fut_2).await {
-            Either::Left(left) => left,
-            Either::Right(right) => right,
+        let fut1 = async {
+            println!("Start first task!");
+            for i in 1..100 {
+                println!("hi number {i} from the first task!");
+                trpl::sleep(Duration::from_millis(0)).await;
+            }
+            println!("End first task!");
         };
 
-        println!("{url} returned first");
-        match maybe_title {
-            Some(title) => println!("Its page title was: '{title}'"),
-            None => println!("It had no title."),
-        }
-    })
-}
-
-async fn page_title(url: &str) -> (&str, Option<String>) {
-    let response_text = trpl::get(url).await.text().await;
-    let title = Html::parse(&response_text)
-        .select_first("title")
-        .map(|title| title.inner_html());
-    (url, title)
+        let fut2 = async {
+            println!("Start second task!");
+            for i in 1..100 {
+                println!("hi number {i} from the second task!");
+                trpl::sleep(Duration::from_millis(0)).await;
+            }
+            println!("End second task!");
+        };
+        trpl::sleep(Duration::from_millis(2000)).await;
+        println!("Hello, world!");
+        trpl::join(fut1, fut2).await;
+    });
 }
