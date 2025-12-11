d0345da593dc716f100504cb043b20d6a392bf85
Augustin Gjini <augustin.gjini@pm.me>
2025-12-11 14:40:33 +0100

    https://doc.rust-lang.org/book/ch17-03-more-futures.html#listing-17-17

--- hello-async/src/main.rs
+++ hello-async/src/main.rs
@@ -1,26 +1,46 @@
 use std::time::Duration;
 
 fn main() {
-    trpl::run(async {
-        let fut1 = async {
-            println!("Start first task!");
-            for i in 1..100 {
-                println!("hi number {i} from the first task!");
-                trpl::sleep(Duration::from_millis(0)).await;
+    trpl::block_on(async {
+        let (tx, mut rx) = trpl::channel();
+
+        let tx1 = tx.clone();
+        let tx1_fut = async move {
+            let vals = vec![
+                String::from("hi"),
+                String::from("from"),
+                String::from("the"),
+                String::from("future"),
+            ];
+
+            for val in vals {
+                tx1.send(val).unwrap();
+                trpl::sleep(Duration::from_millis(500)).await;
             }
-            println!("End first task!");
         };
 
-        let fut2 = async {
-            println!("Start second task!");
-            for i in 1..100 {
-                println!("hi number {i} from the second task!");
-                trpl::sleep(Duration::from_millis(0)).await;
+        let rx_fut = async {
+            while let Some(value) = rx.recv().await {
+                println!("received '{value}'");
             }
-            println!("End second task!");
         };
-        trpl::sleep(Duration::from_millis(2000)).await;
-        println!("Hello, world!");
-        trpl::join(fut1, fut2).await;
+
+        let tx_fut = async move {
+            let vals = vec![
+                String::from("more"),
+                String::from("messages"),
+                String::from("for"),
+                String::from("you"),
+            ];
+
+            for val in vals {
+                tx.send(val).unwrap();
+                trpl::sleep(Duration::from_millis(500)).await;
+            }
+        };
+        let futures: Vec<Box<dyn Future<Output = ()>>> =
+            vec![Box::new(tx1_fut), Box::new(rx_fut), Box::new(tx_fut)];
+
+        trpl::join_all(futures).await;
     });
 }
