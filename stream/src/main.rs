5720bce6d5b5af0e09d359fb715d55a77996a57c
Augustin Gjini <augustin.gjini@pm.me>
2026-01-29 14:02:07 +0100

    https://doc.rust-lang.org/book/ch17-06-futures-tasks-threads.html

--- stream/src/main.rs
+++ stream/src/main.rs
@@ -1,14 +1,32 @@
+use std::pin::{pin, Pin};
+use std::time::Duration;
 use trpl;
 
 fn main() {
     trpl::block_on(async {
-        let tx_fut = async move {
-            // --snip--
-        };
+        let tx_fut = pin!(async move {
+            trpl::sleep(Duration::from_millis(2000)).await;
+        });
 
-        let futures: Vec<Box<dyn Future<Output = ()>>> =
-            vec![Box::new(tx1_fut), Box::new(rx_fut), Box::new(tx_fut)];
+        let tx1_fut = pin!(async move {
+            // --snip--
+        });
+
+        let rx_fut = pin!(async move {
+            trpl::sleep(Duration::from_millis(2000)).await;
+        });
+
+        let futures: Vec<Pin<&mut dyn Future<Output = ()>>> = vec![tx1_fut, rx_fut, tx_fut];
+
+        let bbb = Box::new(Box::new(Box::new(33)));
+        let vvv = Pin::new(vec!["dsd", "sdsdsd"]);
+
+        let ffff = vvv;
+
+        titi(&bbb);
 
         trpl::join_all(futures).await;
-    })
+    });
 }
+
+fn titi(i: &i32) {}
