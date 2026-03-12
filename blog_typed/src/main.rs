69434c0491c958f5e38ae27c8da80fdb06a83160
Augustin Gjini <augustin.gjini@pm.me>
2026-03-12 14:35:44 +0100

    optional post pattern matching https://doc.rust-lang.org/book/ch19-03-pattern-syntax.html

--- blog_typed/src/main.rs
+++ blog_typed/src/main.rs
@@ -11,6 +11,8 @@ fn main() {
 
     assert_eq!("I ate a salad for lunch today", post.content());
     println!("{}", post.content());
+
+    test(Some(post));
 }
 
 fn test(o: Option<Post>) -> i32 {
@@ -18,5 +20,24 @@ fn test(o: Option<Post>) -> i32 {
         return 0;
     };
 
+    let tab = [1, 2, 3, 4];
+
+    let [first, tail @ .., last] = tab;
+
+    println!("{}, {}", first, last);
+    println!("{:?}", tail);
+
+    tab.array_windows()
+        .map(|[a, b]| a + b)
+        .for_each(|sum| println!("{}", sum));
+
     p.content().len() as i32
+}
+
+fn test2(today_post: Option<Post>) -> i32 {
+    if let Some(post) = today_post {
+        post.content().len() as i32
+    } else {
+        0
+    }
 }
