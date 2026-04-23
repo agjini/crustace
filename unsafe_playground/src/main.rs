e8a21426c296966cdc71625ea5f45178a265c1bf
Augustin Gjini <augustin.gjini@pm.me>
2026-04-23 14:02:36 +0200

    Calling Rust Functions from Other Languages

--- unsafe_playground/src/main.rs
+++ unsafe_playground/src/main.rs
@@ -1,19 +1,35 @@
 use std::slice;
+
+unsafe extern "C" {
+    fn abs(input: i32) -> bool;
+}
+
 fn main() {
-    use std::slice;
+    unsafe {
+        println!("Absolute value of -1 according to C: {}", abs(-1));
+    }
 
-    fn split_at_mut(values: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
-        let len = values.len();
-        let ptr = values.as_mut_ptr();
+    split_at_mut(&mut [1, 2, 3, 4, 5], 3);
 
-        assert!(mid <= len);
+    let address = 0x01234usize;
+    let r = address as *mut i32;
 
-        unsafe {
-            let toto = slice::from_raw_parts_mut(ptr, mid);
-            (
-                slice::from_raw_parts_mut(ptr, mid),
-                slice::from_raw_parts_mut(ptr.add(mid), len - mid),
-            )
-        }
+    let values: &[i32] = unsafe { slice::from_raw_parts_mut(r, 10000) };
+
+    // println!("values: {:?}", values);
+}
+
+fn split_at_mut(values: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
+    let len = values.len();
+    let ptr = values.as_mut_ptr();
+
+    assert!(mid <= len);
+
+    unsafe {
+        let toto = slice::from_raw_parts_mut(ptr, mid);
+        (
+            slice::from_raw_parts_mut(ptr, mid),
+            slice::from_raw_parts_mut(ptr.add(mid), len - mid),
+        )
     }
 }
