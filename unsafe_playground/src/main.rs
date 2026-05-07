88993ea85d32e062b66f8f819d627d294bc138ce
Augustin Gjini <augustin.gjini@pm.me>
2026-05-07 15:12:36 +0200

    unsafe++

--- unsafe_playground/src/main.rs
+++ unsafe_playground/src/main.rs
@@ -1,13 +1,22 @@
 use std::slice;
 
 unsafe extern "C" {
-    fn abs(input: i32) -> bool;
+    safe fn abs(input: i32) -> u32;
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn call_from_c() {
+    println!("Just called a Rust function from C!");
+}
+
+#[link(name = "m")] // libm.so → "m" (sans le préfixe lib ni l'extension)
+unsafe extern "C" {
+    fn cos(x: f64) -> f64;
 }
 
 fn main() {
-    unsafe {
-        println!("Absolute value of -1 according to C: {}", abs(-1));
-    }
+    let c = unsafe { cos(0.0) };
+    println!("Absolute value of -1 according to C: {}", abs(-1));
 
     split_at_mut(&mut [1, 2, 3, 4, 5], 3);
 
