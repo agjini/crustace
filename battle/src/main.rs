19a8ce018501f685268cdfaf6b5a76c81bb1c21a
Augustin Gjini <augustin.gjini@gmail.com>
2026-06-18 14:08:12 +0200

    battle

---
+++ battle/src/main.rs
@@ -0,0 +1,84 @@
+use rand::distr::StandardUniform;
+use rand::prelude::Distribution;
+use rand::{Rng, RngExt};
+use std::fmt::{Display, Formatter};
+use std::io;
+use std::io::{Read, stdin};
+
+enum Color {
+    /// Pique
+    Spade,
+
+    /// Coeur
+    Hearth,
+
+    /// Carreau
+    Diamond,
+
+    /// Trefle
+    Club,
+}
+
+struct Card {
+    value: u8,
+    color: Color,
+}
+
+fn main() {
+    let mut rng = rand::rng();
+    let cards = 54;
+
+    loop {
+        println!("Press enter to draw a card");
+        let mut enter = String::new();
+        stdin().read_line(&mut enter).expect("Invalid input.");
+        println!("You entered: -{}-", enter);
+        let player_card = Card {
+            value: rng.random_range(1..11),
+            color: rng.random(),
+        };
+        println!("Votre carte : {player_card}");
+
+        let computer_card = Card {
+            value: rng.random_range(1..11),
+            color: rng.random(),
+        };
+        println!("Alfred carte : {computer_card}");
+
+        if (player_card.value > computer_card.value) {
+            println!("Tu as gagné Youpi GG");
+        } else {
+            println!("Tu as perdu... noob");
+        }
+    }
+}
+
+impl Display for Card {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} de {}", self.value, self.color)
+    }
+}
+
+impl Display for Color {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let char = match self {
+            Color::Club => '♣',
+            Color::Diamond => '♦',
+            Color::Spade => '♠',
+            Color::Hearth => '♥',
+        };
+        write!(f, "{}", char)
+    }
+}
+
+impl Distribution<Color> for StandardUniform {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Color {
+        let random_color = rng.random_range(0..4);
+        match random_color {
+            0 => Color::Spade,
+            1 => Color::Diamond,
+            2 => Color::Hearth,
+            _ => Color::Club,
+        }
+    }
+}
